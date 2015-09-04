61f705152db186e5fb76a5e33e4edee80d698c16
Mikhail Popov <mikhail@mpopov.com>
2015-09-04 11:18:11 -0700

    Fixed the WDQS usage aggregation query after having an enlightening chat with WDQS's engineers.
    Change-Id: I21b7a9a2b2865bb8a192d7b4be43e0b82cbb83be

--- wdqs/basic_usage.R
+++ wdqs/basic_usage.R
@@ -22,18 +22,17 @@ main <- function(date = NULL) {
 
   # Write query and dump to file
   query <- paste0("USE wmf;
-                   SELECT year, month, day,
-                   FIND_IN_SET(uri_path, '/bigdata/namespace/wdq/sparql,/,/index.php') AS uri_path,
-                   IF(INSTR(uri_query, 'query') > 0, 'query', 'other') AS uri_query,
-                   IF(INSTR(content_type, 'sparql-results') > 0, 'sparql results', 'other') AS content_type,
+                   SELECT year, month, day, uri_path,
+                   (floor(cast(http_status as int) / 100) * 100) AS http_category,
                    COUNT(*) AS n
                    FROM webrequest",
                    subquery,
-                  "AND webrequest_source = 'misc' AND FIND_IN_SET(http_status, '200,301,302,303') > 0
-                   GROUP BY year, month, day,
-                   FIND_IN_SET(uri_path, '/bigdata/namespace/wdq/sparql,/,/index.php'),
-                   IF(INSTR(uri_query, 'query') > 0, 'query', 'other'),
-                   IF(INSTR(content_type, 'sparql-results') > 0, 'sparql results', 'other');")
+                  "AND webrequest_source = 'misc'
+                   AND uri_host = 'query.wikidata.org'
+                   AND uri_path IN('/', '/bigdata/namespace/wdq/sparql')
+                   GROUP BY year, month, day, uri_path,
+                   floor(cast(http_status as int) / 100) * 100;")
+                   
   query_dump <- tempfile()
   cat(query, file = query_dump)
 
@@ -43,12 +42,9 @@ main <- function(date = NULL) {
   results <- read.delim(results_dump, sep = "\t", quote = "", as.is = TRUE, header = TRUE)
   file.remove(query_dump, results_dump)
 
-  results$uri_path <- factor(results$uri_path, 0:3, c("other", "/bigdata/namespace/wdq/sparql", "/", "/index.php"))
-
   output <- data.frame(timestamp = as.Date(paste(results$year, results$month, results$day, sep = "-")),
                        path = results$uri_path,
-                       query = results$uri_query,
-                       content = results$content_type,
+                       http_status = results$http_category,
                        events = results$n,
                        stringsAsFactors = FALSE)
 
@@ -58,10 +54,10 @@ main <- function(date = NULL) {
 }
 
 # Backlog (start date: 2015-07-28):
-# backlog <- function(days) {
-#   for (i in days:1) try(main(Sys.Date() - i), silent = TRUE)
-# }; backlog(30) # as of 2015-08-27
+backlog <- function(days) {
+  for (i in days:1) try(main(Sys.Date() - i), silent = TRUE)
+}; backlog(30) # as of 2015-08-27In the 
 
 # Run and kill
-main()
-q(save = "no")
+# main()
+# q(save = "no")
