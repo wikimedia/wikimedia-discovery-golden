f6e969a4b46dc4b6f02ed611385f37b719b6abbf
ironholds <ironholds@gmail.com>
2015-09-07 20:57:19 -0400

    Fixes inability of data retrieval scripts to run
    After the switchover to 'golden' all data retrieval scripts ceased working because the
    new main.sh file didn't include relative paths (or a continuation chain.
    Or a path at all, in one case).
    
    This fixes that, and hopefully it won't occur again. Distinctly from this we
    need to backport the data.
    
    Bug: T111714
    Change-Id: Iac6f784727e2f7b24098b7277c4a637f9f47990b

--- main.sh
+++ main.sh
@@ -1,8 +1,8 @@
-R CMD BATCH search/desktop.R &&
-R CMD BATCH search/mobile.R &&
-R CMD BATCH search/app.R &&
-R CMD BATCH search/api.R &&
-R CMD BATCH search/failures.R &&
-R CMD BATCH wdqs/basic_usage.R
-python core.py &&
+R CMD BATCH ./search/desktop.R &&
+R CMD BATCH ./search/mobile.R &&
+R CMD BATCH ./search/app.R &&
+R CMD BATCH ./search/api.R &&
+R CMD BATCH ./search/failures.R &&
+R CMD BATCH ./wdqs/basic_usage.R &&
+python ./search/core.py &&
 rm -rf .RData
