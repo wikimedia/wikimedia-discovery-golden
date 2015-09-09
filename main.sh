e52677057b03b8a770a50e6fdda73cce9757d735
ironholds <ironholds@gmail.com>
2015-09-09 12:36:56 -0400

    Remove deprecated script
    Removes references to a deprecated and no-longer-existent script,
    which should be the last data collection bug we'll encounter
    for quite some time.
    
    Change-Id: I6838adf0ab12309c222fb2bd4bf5f8ac75c67739

--- main.sh
+++ main.sh
@@ -2,7 +2,6 @@ R CMD BATCH ./search/desktop.R &&
 R CMD BATCH ./search/mobile.R &&
 R CMD BATCH ./search/app.R &&
 R CMD BATCH ./search/api.R &&
-R CMD BATCH ./search/failures.R &&
 R CMD BATCH ./wdqs/basic_usage.R &&
 python ./search/core.py &&
 rm -rf .RData
