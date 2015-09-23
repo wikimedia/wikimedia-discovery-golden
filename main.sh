370cafb95d1c9995c9fffdd7cbddc7f646663a0f
ironholds <ironholds@gmail.com>
2015-09-23 17:36:20 -0400

    Add daily dwell-time calculation
    Bug: 113513
    Change-Id: Iffc68c3ea503bc5bc5744b4733036b4fdac75125

--- main.sh
+++ main.sh
@@ -2,6 +2,7 @@ R CMD BATCH ./search/desktop.R &&
 R CMD BATCH ./search/mobile.R &&
 R CMD BATCH ./search/app.R &&
 R CMD BATCH ./search/api.R &&
+R CMD BATCH ./search/dwelltime.R &&
 R CMD BATCH ./wdqs/basic_usage.R &&
 R CMD BATCH ./maps/usage.R &&
 python ./search/core.py &&
---
+++ search/dwelltime.R
@@ -0,0 +1,35 @@
+# Per-file config:
+base_path <- "/a/aggregate-datasets/search/"
+library(ortiz)
+
+# Common dependencies
+source("common.R")
+
+main <- function(date = NULL, table = "TestSearchSatisfaction2_13223897"){
+  
+  # Retrieve data
+  data <- as.data.frame(query_func(fields = "
+                        SELECT event_searchSessionId AS session_id,
+                        timestamp
+                        ",
+                        date = date,
+                        table = table,
+                        conditionals = "event_action IN('searchResultPage','visitPage')"))
+  data$timestamp <- olivr::from_mediawiki(data$timestamp)
+  
+  # Generate the data
+  if(is.null(date)){
+    date <- as.Date(data$timestamp[1])
+  }
+
+  dwell_data <- ortiz::dwell_time(data = data, ids = "session_id", timestamps = "timestamp", dwell_threshold = 10)
+  
+  # Turn it into a data.frame we can write out conditionally, and then do that
+  output <- data.frame(timestamp = date,
+                       threshold_pass = sum(dwell_data)/length(dwell_data))
+  conditional_write(output, file = file.path(base_path, "search_threshold_pass_rate.tsv"))
+}
+
+# Run and kill
+main()
+q(save = "no")
