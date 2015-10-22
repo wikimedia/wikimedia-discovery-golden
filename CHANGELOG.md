b11c6ba324806aab667f975f061b1daa7b132879
Mikhail Popov <mikhail@mpopov.com>
2015-10-22 14:49:43 -0700

    Fix survival data - Dynamically decides which MySQL table to query depending on date - Makes sure the code is backward compatible in case of pre Oct 13th data - Treats each individual session as its own observation rather than belonging to a set of other sessions by the same user (gets rid of unreliable clientIp+userAgent grouping)
    Bug: T116189
    Change-Id: I16a4722a4c95ab4795bb19642c156c18e1267e55

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,12 @@
 # Change Log (Patch Notes)
 All notable changes to this project will be documented in this file.
 
+## 2015/10/22
+- Fixes LDN (survival) code
+
+## 2015/10/20
+- Refactored to be more robust and backfilling-friendly
+
 ## 2015/09/30
 - Added a change log
 - Added a contributor code of conduct
--- search/LDN.R
+++ search/LDN.R
@@ -1,40 +1,56 @@
 ## Calculates the median lethal dose (LD50) and other.
 ## LD50 = the time point at which we have lost 50% of our users.
 
+# Per-file config:
 base_path <- paste0(write_root, "search/")
 
-main <- function(date = NULL, table = "TestSearchSatisfaction2_13223897") {
+main <- function(date = NULL) {
   
   checkins <- c(0, 10, 20, 30, 40, 50, 60, 90, 120, 150, 180, 210, 240, 300, 360, 420)
+  # ^ this will be used for figuring out the interval bounds for each check-in
   
-  # Ensure we have a date and deconstruct it into a MW-friendly format
+  # Ensure we have a date:
   if(is.null(date)) {
     date <- Sys.Date() - 1
   }
   
-  # Get data and format
+  # Decide which table to query depending on the date:
+  if ( date < as.Date('2015-09-02') ) {
+    warning('No data exists prior to 09/02/2015')
+    return(invisible())
+  } else if ( date >= as.Date('2015-09-02') && date < as.Date('2015-10-13') ) {
+    table <- "TestSearchSatisfaction2_13223897"
+  } else { # date >= 2015-10-13
+    table <- "TestSearchSatisfaction2_14098806"
+  }
+  
+  # Get data and format:
   data <- query_func(fields = "SELECT * ",
                      date = date, table = table)
   data$timestamp <- lubridate::ymd_hms(data$timestamp)
-  data$user_id <- factor(as.numeric(factor(paste0(data$clientIp, data$userAgent, sep='~'))))
-  page_visits <- ddply(data, .(user_id, event_searchSessionId, event_pageId),
+  # Backwards-compatibility:
+  if ( table == "TestSearchSatisfaction2_13223897" ) {
+    names(data) <- sub('event_pageId', 'event_pageViewId', names(data))
+  }
+  # Treat each individual search session as its own thing, rather than belonging
+  #   to a set of other search sessions by the same user.
+  page_visits <- ddply(data, .(event_searchSessionId, event_pageViewId),
                     function(session) {
-                      if (!all(c('visitPage', 'checkin') %in% session$event_action)) return(NULL)
+                      if (!all(c('visitPage', 'checkin') %in% session$event_action)) {
+                        return(NULL)
+                      }
                       temp <- session[all(c('visitPage', 'checkin') %in% session$event_action), ]
                       last_checkin <- max(temp$event_checkin, na.rm = TRUE)
                       idx <- which(checkins > last_checkin)
-                      if (length(idx) == 0) idx <- 16 # length(checkins)
+                      if (length(idx) == 0) idx <- 16 # length(checkins) = 16
                       next_checkin <- checkins[min(idx)]
                       status <- ifelse(last_checkin == 420, 0, 3)
                       return(c(`last check-in` = last_checkin,
                                `next check-in` = next_checkin,
                                status = status))
-                    }) %>%
-    # Some users may have sessions with multiple visited pages, let's pick 1 at random:
-    ddply(.(user_id, event_searchSessionId), olivr::sample_dataframe, size = 1) %>%
-    # Some users may have multiple sessions, let's pick 1 at random:
-    ddply(.(user_id), olivr::sample_dataframe, size = 1) %>%
-    { .[, 4:6] }
+                    }) # %>%
+    # Some sessions may have multiple visited pages, let's pick 1 at random:
+    # ddply(.(user_id, event_searchSessionId), olivr::sample_dataframe, size = 1)
   
   surv <- Surv(time = page_visits$`last check-in`,
                time2 = page_visits$`next check-in`,
@@ -44,6 +60,8 @@ main <- function(date = NULL, table = "TestSearchSatisfaction2_13223897") {
   output <- data.frame(date = date, rbind(quantile(fit, probs = c(0.10, 0.25, 0.50, 0.75, 0.90, 0.95, 0.99))$quantile))
   colnames(output) <- c('date', 'LD10', 'LD25', 'LD50', 'LD75', 'LD90', 'LD95', 'LD99')
   conditional_write(output, file.path(base_path, "sample_page_visit_ld.tsv"))
+  
+  return(invisible())
   
 }
 
