9c7b436967d25ee4ed1165b9b148cc093eb3e7f8
ironholds <ironholds@gmail.com>
2015-12-21 06:42:27 -0500

    Switch Maps data collection over to detect automata
    What it says on the tin. Also moves some filtering to Hive
    and makes some style corrections.
    
    Most of the aggregation logic has been removed for the
    simple reason that keeping it makes it impossible to
    compute automata happily :/.
    
    Bug: T121758
    Change-Id: I878bbe2a78d2e144154a1a654cee795cb89f0f12

--- maps/tiles.R
+++ maps/tiles.R
@@ -16,7 +16,7 @@ main <- function(date = NULL) {
   subquery <- date_clause(date)
 
   # Get the per-user tile usage:
-  query <- paste0("SELECT style, zoom, scale, format, user_id, cache, COUNT(1) AS n
+  query <- paste0("SELECT style, zoom, scale, format, user_id, cache, is_automata, COUNT(1) AS n
                    FROM (
                      SELECT
                        REGEXP_EXTRACT(uri_path, '^/([^/]+)/([0-9]{1,2})/(-?[0-9]+)/(-?[0-9]+)(@([0-9]\\.?[0-9]?)x)?\\.([a-z]+)$', 1) AS style,
@@ -24,40 +24,32 @@ main <- function(date = NULL) {
                        COALESCE(REGEXP_EXTRACT(uri_path, '^/([^/]+)/([0-9]{1,2})/(-?[0-9]+)/(-?[0-9]+)(@([0-9]\\.?[0-9]?)x)?\\.([a-z]+)$', 6), '1') AS scale,
                        REGEXP_EXTRACT(uri_path, '^/([^/]+)/([0-9]{1,2})/(-?[0-9]+)/(-?[0-9]+)(@([0-9]\\.?[0-9]?)x)?\\.([a-z]+)$', 7) AS format,
                        CONCAT(user_agent, client_ip) AS user_id,
-                       cache_status AS cache
+                       cache_status AS cache,
+                       CASE WHEN agent_type = 'spider' THEN 'TRUE' ELSE 'FALSE' END AS is_automata
                      FROM wmf.webrequest", subquery, "
                        AND webrequest_source = 'maps'
                        AND http_status IN('200','304')
                        AND uri_path RLIKE '^/([^/]+)/([0-9]{1,2})/(-?[0-9]+)/(-?[0-9]+)(@([0-9]\\.?[0-9]?)x)?\\.([a-z]+)$'
                        AND uri_query <> '?loadtesting'
                    ) prepared
-                   GROUP BY style, zoom, scale, format, user_id, cache;")
+                   WHERE zoom != '' AND style != ''
+                   GROUP BY style, zoom, scale, format, user_id, cache, is_automata;")
   results <- query_hive(query)
-
-  # In my tests, I've gotten rows with blank style and NA zoom. I have no way of finding out whether
-  #   that's because of the hive problems I've been experiencing or what. In either case, it's worth
-  #   including this line for data sanitation:
-  results <- results[!is.na(results$zoom) & results$style != "", ]
-  # P.S. The zoom sometimes exceeds what we actually allow (18). Yuri said that's acceptable but we
+  
+  # The zoom sometimes exceeds what we actually allow (18). Yuri said that's acceptable but we
   # enlarge the images, so they're not actually getting zoom level 21-26 tiles.
-
-  # Summarise the per-user results  by aggregating across styles and zooms:
-  results <- plyr::ddply(results, plyr::.(style, zoom, scale, format, cache), function(x) {
-    cbind(x[1, c('style' ,'zoom', 'scale', 'format', 'cache'), drop = FALSE],
-          users = length(x$n),
-          total = sum(x$n),
-          average = round(mean(x$n), 2),
-          median = ceiling(median(x$n)),
-          percentile95 = ceiling(quantile(x$n, 0.95)),
-          percentile99 = ceiling(quantile(x$n, 0.99)))
-  })
-
-  # Clean up those results:
-  results <- results[order(results$style, results$zoom, results$scale, results$format, results$cache), ]
   results$date <- date
-  output <- results[, union('date', names(results))]
-
+  output <- as.data.table(results[, union('date', names(results))])
+  with_automata_output <- output[,list(users = length(user_id), total=sum(n), average = round(mean(n)), median = ceiling(median(n)),
+                                       percentile95 = ceiling(quantile(n, 0.95)), percentile99 = ceiling(quantile(n, 0.99))),
+                                 by= setdiff(names(output),c("n","user_id", "is_automata"))]
+  
+  without_automata_output <- output[output$is_automata == FALSE,
+                                    list(users = length(user_id), total=sum(n), average = round(mean(n)), median = ceiling(median(n)),
+                                    percentile95 = ceiling(quantile(n, 0.95)), percentile99 = ceiling(quantile(n, 0.99))),
+                                    by= setdiff(names(output),c("n","user_id", "is_automata"))]
   # Write out
-  conditional_write(output, file.path(base_path, "tile_aggregates.tsv"))
-
+  conditional_write(with_automata_output, file.path(base_path, "tile_aggregates_with_automata.tsv"))
+  conditional_write(without_automata_output, file.path(base_path, "tile_aggregates_no_automata.tsv"))
+  
 }
