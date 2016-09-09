53b5c95ed88ff4ac23075ac62ec8c913c5c100ff
Mikhail Popov <mpopov@wikimedia.org>
2016-09-09 12:37:04 -0700

    Split Portal pageviews by high-volume & low-volume clients
    1. Counts pageviews to wikipedia.org on a per-client IP basis
    2. Finds the top 99.99% to use a threshold
    3. Uses the threshold to count pageviews according to two types of clients
       * high-volume
       * low-volume
    4. Outputs 4 columns:
       * date
       * pageviews (total pageviews)
       * high_volume (total pageviews from clients with >= 99.99th percentile PVs)
       * low_volume (total pageviews from clients with < 99.99th percentile PVs)
       * threshold (99.99th percentile that was used to split the clients)
    
    NOTE: Do not merge until portal_pageviews.tsv has been updated to include
          the columns "high_volume", "low_volume", and "threshold" filled w/
          NAs for dates that cannot be backfilled.
    
    Bug: T143605
    Change-Id: I56a333753770153cea449c5b6cef6b840ecb684a

--- portal/pageviews.R
+++ portal/pageviews.R
@@ -1,26 +1,38 @@
 base_path <- paste0(write_root, "portal/")
 
 main <- function(date = NULL){
-  
+
   # Date subquery
   clause_data <- wmf::date_clause(date)
 
   # Query
   data <- wmf::query_hive(paste("USE wmf;
-                                 SELECT COUNT(*) AS pageviews
+                                 SELECT
+                                   client_ip,
+                                   COUNT(1) AS pageviews
                                  FROM webrequest",
-                                 clause_data$date_clause, 
-                                "AND uri_host RLIKE('^(www\\.)?wikipedia.org/*$')
-                                 AND INSTR(uri_path, 'search-redirect.php') = 0
-                                 AND content_type RLIKE('^text/html')
-                                 AND webrequest_source = 'text'
-                                 AND NOT (referer RLIKE('^http://localhost'))
-                                 AND agent_type = 'user'
-                                 AND referer_class != 'unknown'
-                                 AND http_status IN('200', '304');"))
-      
-  output <- data.frame(date = clause_data$date, pageviews = data$pageviews)
-  
+                                 clause_data$date_clause, "
+                                   AND uri_host RLIKE('^(www\\.)?wikipedia.org/*$')
+                                   AND INSTR(uri_path, 'search-redirect.php') = 0
+                                   AND content_type RLIKE('^text/html')
+                                   AND webrequest_source = 'text'
+                                   AND NOT (referer RLIKE('^http://localhost'))
+                                   AND agent_type = 'user'
+                                   AND referer_class != 'unknown'
+                                   AND http_status IN('200', '304')
+                                 GROUP BY client_ip;"))
+  data <- data.table::as.data.table(data[!is.na(data$pageviews), ])
+  `99.99th percentile` <- floor(quantile(data$pageviews, 0.9999))
+  data$client_type <- ifelse(data$pageviews < `99.99th percentile`, "low_volume", "high_volume")
+
+  output <- cbind(date = clause_data$date,
+                  tidyr::spread(data[, list(pageviews = sum(pageviews)), by = "client_type"],
+                                client_type, pageviews),
+                  threshold = `99.99th percentile`)
+  output$pageviews = output$high_volume + output$low_volume
+
   #Return!
-  wmf::write_conditional(output, file.path(base_path, "portal_pageviews.tsv"))
+  wmf::write_conditional(output[, c("date", "pageviews", "high_volume", "low_volume", "threshold")],
+                         file.path(base_path, "portal_pageviews.tsv"))
+
 }
