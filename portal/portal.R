10324ebd310e3d473922bf0e5817ad704fcd1eb7
Chelsy Xie <cxie@wikimedia.org>
2016-09-22 17:24:24 -0700

    Ungroup last_action_country and first_visits_country
    I checked the function wmf::rewrite_conditional and sink file run.Rout at /a/discovery/golden, but couldn't find anything suspicious... Since most_common_country got no problem when updated, and "ungroup()" seems to be the main difference between it and the other two, I made this change to test if it can fix the bug. But I'm not sure why...
    
    Bug: T146422
    Change-Id: I914bb0a42afd5de0ec52fa1bfb6beb8067aa500b

--- portal/portal.R
+++ portal/portal.R
@@ -153,7 +153,8 @@ main <- function(date = NULL, table = "WikipediaPortal_15890769"){
     dplyr::filter(!duplicated(session, fromLast = TRUE)) %>%
     dplyr::group_by(date, section_used, country) %>%
     dplyr::summarize(events = n()) %>%
-    dplyr::mutate(proportion = round(events/sum(events), 4))
+    dplyr::mutate(proportion = round(events/sum(events), 4)) %>%
+    dplyr::ungroup()
 
   # Most common section clicked by country
   most_common_country <- data_w_countryname %>%
@@ -178,7 +179,8 @@ main <- function(date = NULL, table = "WikipediaPortal_15890769"){
     dplyr::filter(visit == 1) %>%
     dplyr::group_by(date, section_used, country) %>%
     dplyr::summarize(sessions = n()) %>%
-    dplyr::mutate(proportion = round(sessions/sum(sessions), 4))
+    dplyr::mutate(proportion = round(sessions/sum(sessions), 4)) %>%
+    dplyr::ungroup()
 
   # Get user agent data
   wmf::set_proxies() # To allow for the latest YAML to be retrieved.
