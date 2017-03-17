e4c66003b835290a9c2593e8452bf6e40c84a6c3
Mikhail Popov <mpopov@wikimedia.org>
2017-03-17 10:36:15 -0700

    Adds permission and submodule checking
    "You better check yo self before you wreck yo self"
    
    Bug: T160772
    Change-Id: Ia53e8826af757fa4435c71273d6d9eac86864c23

--- main.sh
+++ main.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Check if modules/forecasts/forecast.R has execution permission for Reportupdater
+# (If it doesn't, then other R and shell scripts in modules/ probably don't either.)
+if [ `ls -l modules/forecasts | grep -e forecast.R | grep -e "-rwxrwxr-x" | wc -l` == "0" ]; then
+  echo "Warning: modules do not have execution permission; granting now..."
+  chmod +x -R modules/
+fi
+
+# Check if Reportupdater git submodule is set up
+if [ ! -f reportupdater/update_reports.py ]; then
+  echo "Warning: Reportupdater needs to be initialized and updated..."
+  git submodule init && git submodule update
+fi
+
 # Metrics
 for module in "external_traffic" "wdqs" "maps" "search" "portal"
 do
