5cdc2c998aead9a33acee1ebb78ec1803092d895
Chelsy Xie <cxie@wikimedia.org>
2017-04-05 15:50:40 -0700

    Get browser info from new userAgent field
    Bug: T162178
    Change-Id: Ib292a1b87338c596125618b29ad16b7a82e48141

--- modules/metrics/portal/user_agents.R
+++ modules/metrics/portal/user_agents.R
@@ -53,7 +53,16 @@ if (nrow(results) == 0) {
   # Get user agent data
   wmf::set_proxies() # To allow for the latest YAML to be retrieved.
   uaparser::update_regexes()
-  ua_data <- data.table::as.data.table(uaparser::parse_agents(results$user_agent, fields = c("browser", "browser_major")))
+  ua_data <- data.table::rbindlist(lapply(results$user_agent, function(x){
+    if (grepl("^\\{", x)){
+      temp <- unlist(jsonlite::fromJSON(x)[c("browser_family", "browser_major")])
+      names(temp)[1] <- "browser"
+      temp <- as.data.frame(as.list(temp))
+      return(temp)
+    } else {
+      return(uaparser::parse_agents(x, fields = c("browser", "browser_major")))
+    }
+  }), fill = TRUE)
   ua_data <- ua_data[, j = list(amount = .N), by = c("browser", "browser_major")]
   ua_data$date <- results$date[1]
   ua_data$percent <- round((ua_data$amount/sum(ua_data$amount)) * 100, 2)
