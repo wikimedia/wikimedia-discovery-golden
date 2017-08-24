55a7cfe80dc4f5b20590191ead4e127a1000a64e
Mikhail Popov <mpopov@wikimedia.org>
2017-08-24 11:43:23 -0700

    Enable finer logging
    Change-Id: I0f5aa7a41f20ae6db8236801e656e6cb23a47ece

--- main.sh
+++ main.sh
@@ -10,12 +10,12 @@ rsync -c docs/README.md /srv/published-datasets/discovery/README.md
 for module in "external_traffic" "wdqs" "maps" "search" "portal"
 do
  echo "Running Reportupdater on ${module} metrics..."
- nice ionice reportupdater/update_reports.py "modules/metrics/${module}" "/srv/published-datasets/discovery/metrics/${module}"
+ nice ionice reportupdater/update_reports.py -l info "modules/metrics/${module}" "/srv/published-datasets/discovery/metrics/${module}"
 done
 
 # Forecasts (dependent on latest metrics)
 for module in "search" "wdqs"
 do
  echo "Running Reportupdater on ${module} forecasts..."
- nice -n 17 ionice -c 2 -n 6 reportupdater/update_reports.py "modules/forecasts/${module}" "/srv/published-datasets/discovery/forecasts/${module}"
+ nice -n 17 ionice -c 2 -n 6 reportupdater/update_reports.py -l info "modules/forecasts/${module}" "/srv/published-datasets/discovery/forecasts/${module}"
 done
