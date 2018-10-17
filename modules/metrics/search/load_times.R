bebfb106fb354249295dbd5f0c2b888b7ccf1500
Chelsy Xie <cxie@wikimedia.org>
2018-10-16 20:17:54 -0700

    Refactor load_times.R to avoid using beeline to query
    Bug: T197128
    Change-Id: I8c8ff049a533a0e025f95692107a886c8c0fd13e

--- modules/metrics/search/load_times.R
+++ modules/metrics/search/load_times.R
@@ -5,6 +5,7 @@ source("config.R")
 suppressPackageStartupMessages({
   library("methods")
   library("optparse")
+  library("magrittr") # Required for piping
 })
 
 option_list <- list(
@@ -28,66 +29,85 @@ if (!(opt$platform %in% c("desktop", "mobileweb", "app"))) {
 # Build query:
 date_clause <- as.character(as.Date(opt$date), format = "year = %Y AND month = %m AND day = %d")
 
-# All three platforms will yield data structured the same way, having the following columns:
-# date      : the date of the event
-# dt : timestamp (ISO-8601) of the event
-# platform  : 'Android' or 'iOS' for app-based events, NULL otherwise
-# event_id  : unique event ID for de-duplication of desktop-based events, NULL o.w.
-# load_time : load time (ms)
 query <- switch(
   opt$platform,
 
   # Desktop-based events:
   desktop = paste0("
-SET mapred.job.queue.name=nice;
-USE event;
-SELECT
-  '", opt$date, "' AS date, dt,
-  'desktop' AS platform,
-  event.uniqueId AS event_id,
-  CASE WHEN event.msToDisplayResults <= 0 THEN NULL
-       ELSE event.msToDisplayResults END AS load_time
-FROM testsearchsatisfaction2
-WHERE ", date_clause, "
-  AND event.action = 'searchResultPage'
-  AND (event.subTest IS NULL OR event.subTest IN ('null', 'baseline'))
-  AND event.source = 'fulltext';"),
+    SET mapred.job.queue.name=nice;
+    USE event;
+    SELECT '", opt$date, "' AS date,
+      'desktop' AS platform,
+      round(percentile(load_time, 0.5), 2) AS median,
+      round(percentile(load_time, 0.95), 2) AS percentile_95,
+      round(percentile(load_time, 0.99), 2) AS percentile_99
+    FROM
+    (SELECT
+      dt,
+      event.uniqueId AS event_id,
+      CASE WHEN event.msToDisplayResults <= 0 THEN NULL
+           ELSE event.msToDisplayResults END AS load_time
+    FROM testsearchsatisfaction2
+    WHERE ", date_clause, "
+      AND event.action = 'searchResultPage'
+      AND (event.subTest IS NULL OR event.subTest IN ('null', 'baseline'))
+      AND event.source = 'fulltext') AS all
+    RIGHT JOIN
+    (SELECT event.uniqueId AS event_id,
+       MIN(dt) AS dt
+    FROM testsearchsatisfaction2
+    WHERE ", date_clause, "
+      AND event.action = 'searchResultPage'
+      AND (event.subTest IS NULL OR event.subTest IN ('null', 'baseline'))
+      AND event.source = 'fulltext'
+    GROUP BY event.uniqueId) AS dedup
+    ON (all.dt=dedup.dt AND all.event_id=dedup.event_id)
+    ;"),
 
   # Mobile Web-based events:
   mobileweb = paste0("
-SET mapred.job.queue.name=nice;
-USE event;
-SELECT
-  '", opt$date, "' AS date, dt,
-  'mobileweb' AS platform,
-  'N/A' AS event_id,
-  CASE WHEN event.timeToDisplayResults <= 0 THEN NULL
-       ELSE event.timeToDisplayResults END AS load_time
-FROM mobilewebsearch
-WHERE ", date_clause, "
-  AND event.action = 'impression-results';"),
+    SET mapred.job.queue.name=nice;
+    USE event;
+    SELECT
+      '", opt$date, "' AS date,
+      'mobileweb' AS platform,
+      round(percentile(CASE WHEN event.timeToDisplayResults <= 0 THEN NULL
+           ELSE event.timeToDisplayResults END, 0.5), 2) AS median,
+      round(percentile(CASE WHEN event.timeToDisplayResults <= 0 THEN NULL
+           ELSE event.timeToDisplayResults END, 0.95), 2) AS percentile_95,
+      round(percentile(CASE WHEN event.timeToDisplayResults <= 0 THEN NULL
+           ELSE event.timeToDisplayResults END, 0.99), 2) AS percentile_99
+    FROM mobilewebsearch
+    WHERE ", date_clause, "
+      AND event.action = 'impression-results';"),
 
   # App-based events:
   app = paste0("
-SET mapred.job.queue.name=nice;
-USE event;
-SELECT
-  '", opt$date, "' AS date, dt,
-  useragent.os_family AS platform,
-  'N/A' as event_id,
-  CASE WHEN COALESCE(event.timeToDisplayResults, event.time_to_display_results) <= 0 THEN NULL
-       ELSE COALESCE(event.timeToDisplayResults, event.time_to_display_results) END AS load_time
-FROM mobilewikiappsearch
-WHERE ", date_clause, "
-  AND event.action = 'results'
-  -- Need to union with MobileWikiAppiOSSearch after T205551 is fixed
-  ;")
+    SET mapred.job.queue.name=nice;
+    USE event;
+    SELECT
+    '", opt$date, "' AS date,
+    platform,
+    round(percentile(load_time, 0.5), 2) AS median,
+    round(percentile(load_time, 0.95), 2) AS percentile_95,
+    round(percentile(load_time, 0.99), 2) AS percentile_99
+    FROM (
+      SELECT
+      useragent.os_family AS platform,
+      CASE WHEN COALESCE(event.timeToDisplayResults, event.time_to_display_results) <= 0 THEN NULL
+           ELSE COALESCE(event.timeToDisplayResults, event.time_to_display_results) END AS load_time
+    FROM mobilewikiappsearch
+    WHERE ", date_clause, "
+      AND event.action = 'results'
+      -- Need to union with MobileWikiAppiOSSearch after T205551 is fixed
+    ) AS all_serp
+    GROUP BY platform;")
 
 )
 
 # Fetch data from database using Hive:
 results <- tryCatch(
-  suppressMessages(wmf::query_hive(query, use_beeline = TRUE)),
+  suppressMessages(wmf::query_hive(query)),
   error = function(e) {
     return(data.frame())
   }
@@ -113,25 +133,12 @@ if (nrow(results) == 0) {
     )
   }
 } else {
-  results$dt <- lubridate::ymd_hms(results$dt)
-  results$load_time <- as.numeric(gsub("NULL", NA, results$load_time, fixed = TRUE))
-  # Remove duplicated events on TSS2:
-  if (opt$platform == "desktop") {
-    results <- results[order(results$event_id, results$dt), ]
-    results <- results[!duplicated(results$event_id), ]
-  }
-  results$event_id <- NULL
-  library(magrittr) # Required for piping
-  # Process load times:
   output <- results %>%
-    dplyr::group_by(date, platform) %>%
-    dplyr::summarize(
-      Median = round(quantile(load_time, 0.5, na.rm = TRUE), 2),
-      `95th percentile` = round(quantile(load_time, 0.95, na.rm = TRUE), 2),
-      `99th percentile` = round(quantile(load_time, 0.99, na.rm = TRUE), 2)
-    ) %>%
-    dplyr::ungroup() %>%
-    as.data.frame
+    dplyr::rename(
+      Median = median,
+      `95th percentile` = percentile_95,
+      `99th percentile` = percentile_99
+      )
   # Remove platform column for non-app load times:
   if (opt$platform != "app") {
     output$platform <- NULL
