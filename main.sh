cfd2d5040b748307737f2edfb3434f69e61e6806
Luca Toscano <ltoscano@wikimedia.org>
2019-12-04 15:58:44 +0100

    main.sh: add full paths to commands
    What:
    Add full command paths to the main.sh script.
    
    Why:
    This script will be called by the ExecStart of a Systemd unit. To avoid
    adding an explicit WorkingPath (that is not supported in puppet for the moment)
    it seems easier to just have full command paths in main.sh.
    
    Change-Id: I4f0cac49888ab1665bcf3211e6628bd6a4f00e5e

--- main.sh
+++ main.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+DEPLOYMENT_DIR="/srv/discovery/golden"
+DATASETS_DIR="/srv/published-datasets/discovery"
+
 # files created / touched by report updater need to be rw for user and group
 umask 002
 
 # Sync README
-rsync -c docs/README.md /srv/published-datasets/discovery/README.md
+/usr/bin/rsync -c $DEPLOYMENT_DIR/docs/README.md $DATASETS_DIR/README.md
 
 # Metrics
 for module in "external_traffic" "wdqs" "maps" "search"
 do
  echo "Running Reportupdater on ${module} metrics..."
- nice ionice reportupdater/update_reports.py -l info "modules/metrics/${module}" "/srv/published-datasets/discovery/metrics/${module}"
+ /usr/bin/nice /usr/bin/ionice $DEPLOYMENT_DIR/reportupdater/update_reports.py -l info "${DEPLOYMENT_DIR}/modules/metrics/${module}" "${DATASETS_DIR}/metrics/${module}"
 done
