df3d2d42edbdb141b9923efd77694ca9a7cb255c
David Causse <dcausse@wikimedia.org>
2020-05-07 09:58:03 +0200

    Stop generating search datasets
    shiny dashboards are broken and one of the queries is taking
    too much resources on the hadoop cluster (volume search requests/type).
    
    Change-Id: I6a8d7db2c666c52e244e02aa1e4864a2a2c8b31c

--- main.sh
+++ main.sh
@@ -10,7 +10,7 @@ umask 002
 /usr/bin/rsync -c $DEPLOYMENT_DIR/docs/README.md $DATASETS_DIR/README.md
 
 # Metrics
-for module in "external_traffic" "wdqs" "maps" "search"
+for module in "external_traffic" "wdqs" "maps"
 do
  echo "Running Reportupdater on ${module} metrics..."
  /usr/bin/nice /usr/bin/ionice $DEPLOYMENT_DIR/reportupdater/update_reports.py -l info "${DEPLOYMENT_DIR}/modules/metrics/${module}" "${DATASETS_DIR}/metrics/${module}"
